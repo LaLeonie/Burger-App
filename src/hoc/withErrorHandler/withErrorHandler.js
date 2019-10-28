7786b187b4a36606f3a8e09eeb1e99411ed5c0c0
Leonie Zorzi <Leonie.Zorzi@hotmail.com>
2019-10-28 15:09:04 +0000

    Sets ups Checkout component and Routes

--- src/hoc/withErrorHandler/withErrorHandler.js
+++ src/hoc/withErrorHandler/withErrorHandler.js
@@ -8,16 +8,21 @@ const withErrorHandler = (WrappedComponent, axios) => {
       error: null
     };
     componentWillMount() {
-      axios.interceptors.request.use(req => {
+      this.reqInterceptor = axios.interceptors.request.use(req => {
         this.setState({ error: null });
         return req;
       });
-      axios.interceptors.response.use(
+      this.resInterceptor = axios.interceptors.response.use(
         res => res,
         error => {
           this.setState({ error: error });
         }
       );
+    }
+
+    componentWillUnmount() {
+      axios.interceptors.request.eject(this.reqInterceptor);
+      axios.interceptors.request.eject(this.resInterceptor);
     }
 
     errorConfirmedHandler = () => {
