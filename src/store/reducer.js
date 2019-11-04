d449f92fad09eb734568532030fde36122dc279a
Leonie Zorzi <Leonie.Zorzi@hotmail.com>
2019-11-04 09:38:49 +0100

    Finishes add ingredients and remove ingredients functionality in reducer.js

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -8,9 +8,21 @@ const initialState = {
 const reducer = (state = initialState, action) => {
   switch (action.type) {
     case actionTypes.ADD_INGREDIENT:
-      return {};
+      return {
+        ...state,
+        ingredients: {
+          ...state.ingredients,
+          [action.ingredientName]: state.ingredients[action.ingredientName] + 1
+        }
+      };
     case actionTypes.REMOVE_INGREDIENT:
-      return {};
+      return {
+        ...state,
+        ingredients: {
+          ...state.ingredients,
+          [action.ingredientName]: state.ingredients[action.ingredientName] - 1
+        }
+      };
     default:
       return state;
   }
