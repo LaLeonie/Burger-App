70f9c43662ff985be118dd38d44612bd12e42c55
Leonie Zorzi <Leonie.Zorzi@hotmail.com>
2019-11-05 16:54:01 +0000

    Adds totalPrice to burgerBuilder reducer to reset price after purchase

--- src/store/reducers/burgerBuilder.js
+++ src/store/reducers/burgerBuilder.js
@@ -36,7 +36,13 @@ const reducer = (state = initialState, action) => {
     case actionTypes.SET_INGREDIENTS:
       return {
         ...state,
-        ingredients: action.ingredients,
+        ingredients: {
+          salad: action.ingredients.salad,
+          bacon: action.ingredients.bacon,
+          cheese: action.ingredients.cheese,
+          meat: action.ingredients.meat
+        },
+        totalPrice: 4,
         error: false
       };
     case actionTypes.FETCH_INGREDIENTS_FAILED:
