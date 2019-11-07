76cbf0bd67977b545dffe29cb133fbbc2a4075a9
Leonie Zorzi <Leonie.Zorzi@hotmail.com>
2019-11-07 16:36:45 +0000

    Hides Orders when user not logged in

--- src/components/Navigation/NavigationItems/NavigationItems.js
+++ src/components/Navigation/NavigationItems/NavigationItems.js
@@ -7,7 +7,10 @@ const navigationItems = props => (
     <NavigationItem link="/" exact>
       BurgerBuilder
     </NavigationItem>
-    <NavigationItem link="/orders">Orders</NavigationItem>
+    {props.isAuthenticated ? (
+      <NavigationItem link="/orders">Orders</NavigationItem>
+    ) : null}
+
     {props.isAuthenticated ? (
       <NavigationItem link="/logout">Logout</NavigationItem>
     ) : (
