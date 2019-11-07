57b6609b5c061405a9fbefe3761fcedeb10935ce
Leonie Zorzi <Leonie.Zorzi@hotmail.com>
2019-11-07 17:36:57 +0000

    Adds auth data to localStorage and removes it from localStorage on checkout

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -23,6 +23,8 @@ export const authFail = error => {
 };
 
 export const logout = () => {
+  localStorage.removeItems("token");
+  localStorage.removeItems("expirationDate");
   return {
     type: actionTypes.AUTH_LOGOUT
   };
@@ -54,6 +56,11 @@ export const auth = (email, password, isSignup) => {
       .post(url, authData)
       .then(response => {
         console.log(response);
+        const expirationDate = new Date(
+          new Date().getTime() + response.data.expiresIn * 1000
+        );
+        localStorage.setItem("token", response.data.idToken);
+        localStorage.setItem("expirationDate", expirationDate);
         dispatch(authSuccess(response.data.idToken, response.data.localId));
         dispatch(checkAuthTimeout(response.data.expiresIn));
       })
@@ -68,5 +75,21 @@ export const setAuthRedirectPath = path => {
   return {
     type: actionTypes.SET_AUTH_REDIRECT_PATH,
     path: path
+  };
+};
+
+export const authCheckState = () => {
+  return dispatch => {
+    const token = localStorage.getItem("token");
+    if (!token) {
+      dispatch(logout());
+    } else {
+      const expirationDate = new Date(localStorage.getIten("expirationDate"));
+      if (expirationDate > new Date()) {
+        dispatch(authSuccess());
+      } else {
+        dispatch(logout());
+      }
+    }
   };
 };
