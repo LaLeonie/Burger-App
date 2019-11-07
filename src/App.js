c847c33526b3858610b91463a76780b795bc059e
Leonie Zorzi <Leonie.Zorzi@hotmail.com>
2019-11-07 16:34:43 +0000

    Adds Logout Link

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Layout from "./hoc/Layout/Layout";
 import BurgerBuilder from "./containers/BurgerBuilder/BurgerBuilder";
 import Checkout from "./containers/Checkout/Checkout";
 import Orders from "./containers/Orders/Orders";
+import Logout from "./containers/Auth/Logout/Logout";
 
 class App extends Component {
   render() {
@@ -16,6 +17,7 @@ class App extends Component {
             <Route path="/auth" component={Auth} />
             <Route path="/checkout" component={Checkout} />
             <Route path="/orders" component={Orders} />
+            <Route path="/logout" component={Logout} />
             <Route path="/" exact component={BurgerBuilder} />
           </Switch>
         </Layout>
